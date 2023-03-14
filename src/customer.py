dd08b6c21ab26a294feb52816bb82ec145ed087a
BridgeCrossing <dfeelz360@gmail.com>
2023-03-14 18:15:43 -0500

    updated customer class with str requirements and upper cased name, validated with testing

--- src/customer.py
+++ src/customer.py
@@ -1,9 +1,9 @@
 from src.index import store
 
 class Customer:
-    def __init__(self, first_name, last_name):
-        self._first_name = first_name
-        self._last_name = last_name
+    def __init__(self, first_name = str, last_name = str):
+        self._first_name = first_name.upper()
+        self._last_name = last_name.upper()
         self.id = len(store['customers']) + 1
         store['customers'][self.id] = self
 
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -3,8 +3,11 @@ from src.customer import Customer
 
 def test_initialize_customer():
     cher = Customer('cher', 'hwang')
+    assert store['customers'] == {1: cher}
+    assert cher.__dict__ == {'id': 1, '_first_name': 'CHER', '_last_name': 'HWANG'}
+
+def test_customer_update_with_setter():
+    cher = Customer('cher', 'hwang')
     cher.first_name = 'yi-hsuan'
     cher.last_name = 'huang'
-    breakpoint()
-    assert store['customers'] == {1: cher}
-    assert cher.__dict__ == {'id': 1, '_first_name': 'YI-HSUAN', '_last_name': 'HUANG'}
+    assert cher.__dict__ == {'id': 2, '_first_name': 'YI-HSUAN', '_last_name': 'HUANG'}
